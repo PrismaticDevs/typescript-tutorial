302dd941d27ee6b4dadbd1c7c500f78ab7c59e45
Matthew Brignola <matthewbrignola@ymail.com>
2022-05-12 16:50:12 -0600

    need to add all the functions for the buttons

---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Todo } from "./Model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo } : todo)));
+  };
+  return (
+    <>
+      <form className="todos__single">
+        <span className="todos__single--text">{todo.todo}</span>
+        <div>
+          <span className="icon">
+            <AiFillEdit />
+          </span>
+          <span className="icon">
+            <AiFillDelete />
+          </span>
+          <span className="icon" onClick={() => handleDone(todo.id)}>
+            <MdDone />
+          </span>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default SingleTodo;
