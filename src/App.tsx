5f81fc908b4548bd69113a687dfad8aa39d1a9ae
Matthew Brignola <matthewbrignola@ymail.com>
2022-05-13 16:56:19 -0600

    update app.js header

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      <span className="heading">Task Tracker</span>
+      <span className="heading">Typescript Todos</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
       <TodoList todos={todos} setTodos={setTodos} />
     </div>
